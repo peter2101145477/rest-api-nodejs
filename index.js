b7eaacb9c3847e4202d06f3a4745128d1254f3fc
CUI XIU FAN <2101145477po@cyber-u.ac.jp>
2024-06-09 11:10:48 +0900

    Update Users List

--- index.js
+++ index.js
@@ -4,14 +4,30 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-// JSONPlaceholderのAPIエンドポイント
+// JSONPlaceholder的API端点
 const API_URL = 'https://jsonplaceholder.typicode.com/users';
 
-// ユーザー情報を取得するエンドポイント
+// 用户信息获取端点
 app.get('/users', async (req, res) => {
 	try {
 		const response = await axios.get(API_URL);
-		res.json(response.data);
+		const users = response.data;
+		let html = `<h1>User List</h1>`;
+		html += `<ul>`;
+		users.forEach(user => {
+			html += `<li>
+                <p><strong>Name:</strong> ${user.name}</p>
+                <p><strong>Username:</strong> ${user.username}</p>
+                <p><strong>Email:</strong> ${user.email}</p>
+                <p><strong>Address:</strong> ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</p>
+                <p><strong>Phone:</strong> ${user.phone}</p>
+                <p><strong>Website:</strong> <a href="http://${user.website}">${user.website}</a></p>
+                <p><strong>Company:</strong> ${user.company.name}</p>
+                <p><strong>Catchphrase:</strong> ${user.company.catchPhrase}</p>
+            </li>`;
+		});
+		html += `</ul>`;
+		res.send(html);
 	} catch (error) {
 		res.status(500).send('Error fetching users');
 	}
